c19fb3253c92f0a5cc8215e741381d4cfd0dbf66
Marcos Paulo <itsolutions031250@gmail.com>
2022-09-14 16:15:52 -0300

    exemplos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import entities.OrderItem;
+import entities.Product;
+
 public class Program {
     public static void main(String[] args) {
-        
+
+        Product p = new Product("TV", 1000.0);
+
+        OrderItem oi1 = new OrderItem(1, 1000.0, p);
+
+        System.out.println(oi1.getProduct().getName());
     }
 }
